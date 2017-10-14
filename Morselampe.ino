b5574c1d90d9e8af77cc0778c4e8d9251e4892a4
hortenbach <rabbit@posteo.de>
2017-10-14 22:53:16 +0200

    update numbers and serial. untested

--- Morselampe.ino
+++ Morselampe.ino
@@ -7,7 +7,10 @@ int dotLen = 100;     		// length of the morse code 'dot'
 int dashLen = dotLen * 3;    	// length of the morse code 'dash'
 int Spaces = dotLen * 3;     	// length of the spaces between characters
 int Pause = dotLen * 7;  	// length of the pause between words
-int input = 0;
+char input;
+String inputString = "";
+bool stringComplete = false;
+
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -21,12 +24,34 @@ void setup() {
 void loop() 
 {
 	// alternative: while(Serial.available())
-	if (Serial)
+	if (stringComplete)
 	{
-		input = Serial.read();
-		GetChar(input);	
 		Serial.print("received: %c/n", input);
+		for(int i; i<inputString.length(); i++)
+		{
+			inputString.toUpperCase();
+			toMorse(inputString.charAt(i));
+		}
 	}
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char input = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
 }
 
 // DOT
@@ -52,7 +77,7 @@ void Space()
 }
 
 // *** Characters to Morse Code Conversion *** //
-void GetChar(char tmpChar)
+void toMorse(char tmpChar)
 {
 	tmpChar.toUpperCase();
 	switch (tmpChar) {
